cefd4688db31dc1bd378ae78e09a6d0f06fa3efc
d4nielj <d4niel.djm@gmail.com>
2021-11-15 12:04:32 -0500

    Add basic routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
+  root 'application#index'
+
+  resources :users do
+    resources :foods
+    resources :recipes
+  end
+
+  resources :recipe_foods # create -> redirect_to(user, recipe)
+
+  get 'general_shopping_list', to: 'shopping#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
