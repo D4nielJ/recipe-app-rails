81da3789ea8078eb07a9eff56bce73f57689f2e2
d4nielj <d4niel.djm@gmail.com>
2021-11-15 12:17:11 -0500

    Add controllers and index views

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  root 'application#index'
+  root 'public#index'
 
-  resources :users do
-    resources :foods
-    resources :recipes
-  end
+  resources :users
+  resources :foods
+  resources :recipes
 
   resources :recipe_foods # create -> redirect_to(user, recipe)
 
-  get 'general_shopping_list', to: 'shopping#index'
+  get 'shopping', to: 'shopping#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
