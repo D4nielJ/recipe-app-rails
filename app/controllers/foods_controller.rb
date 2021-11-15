a7b2ae9929417cf2bb968032c8705e5df16472ff
d4nielj <d4niel.djm@gmail.com>
2021-11-15 17:21:05 -0500

    Add form to create food and controller method

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,29 @@ class FoodsController < ApplicationController
   def index
     @foods = Food.all
   end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    user = current_user
+    food = user.foods.new(food_params)
+
+    if food.save
+      flash[:notice] = 'Ingredient added it correctly'
+      redirect_to foods_path
+    else
+      flash[:error] = food.errors.messages
+      render :new
+    end
+  end
+
+  def destroy; end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measure_unit, :price)
+  end
 end
