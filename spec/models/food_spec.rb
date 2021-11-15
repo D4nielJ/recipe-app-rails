da1d4a28aabd96b5052e9e4b1e6106cbd4765227
d4nielj <d4niel.djm@gmail.com>
2021-11-15 18:47:19 -0500

    Add spec tests for food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Food, type: :model do
   let!(:user) { create(:user) }
+
+  describe 'Food model' do
+    before :each do
+      @food = build(:food)
+      @food.user = user
+    end
+
+    it 'should be valid' do
+      expect(@food).to be_valid
+    end
+
+    it 'should not be valid with empty fields' do
+      @food.name = nil
+      expect(@food).to_not be_valid
+    end
+
+    it 'should not be valid with empty fields' do
+      @food.measure_unit = nil
+      expect(@food).to_not be_valid
+    end
+
+    it 'should not be valid with empty fields' do
+      @food.price = nil
+      expect(@food).to_not be_valid
+    end
+
+    it 'should not be valid with incorrect string for measure units' do
+      @food.measure_unit = 'not a valid string'
+      expect(@food).to_not be_valid
+    end
+  end
 end
