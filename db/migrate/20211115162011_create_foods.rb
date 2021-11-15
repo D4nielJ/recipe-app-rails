fddd3e0c744e72235124a26754dc90b08ba5a62e
d4nielj <d4niel.djm@gmail.com>
2021-11-15 15:22:58 -0500

    Generate seeds

--- db/migrate/20211115162011_create_foods.rb
+++ db/migrate/20211115162011_create_foods.rb
@@ -3,7 +3,7 @@ class CreateFoods < ActiveRecord::Migration[6.1]
     create_table :foods do |t|
       t.string :name
       t.string :measure_unit
-      t.integer :price
+      t.decimal :price
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
