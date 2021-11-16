0f1de7cf8848ef455be5d2a57fd30478efa80d4e
leonardo <leonardodias@ucl.br>
2021-11-16 10:57:01 -0300

    Fix Recipe.find method to where(user_id).

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,8 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @recipes = Recipe.all
+    user = current_user
+    @recipes = Recipe.where(user_id: user.id)
   end
 
   def new
