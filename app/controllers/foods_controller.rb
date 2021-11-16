4b789b62fd548dad46a2bb7de4109b2635efe3ea
leonardo <leonardodias@ucl.br>
2021-11-16 08:28:13 -0300

    Merge conflicts from feature/foods-list branch

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,42 @@
 class FoodsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @user = current_user
+    @foods = Food.where(user_id: @user.id)
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    user = current_user
+    food = user.foods.new(food_params)
+
+    if food.save
+      flash[:notice] = 'Ingredient added it correctly'
+      redirect_to foods_path
+    else
+      flash[:error] = food.errors.messages
+      render :new
+    end
+  end
+
+  def destroy
+    food = Food.find(params[:id])
+    if food.destroy
+      flash[:notice] = 'Ingredient removed'
+      redirect_to foods_path
+    else
+      flash[:error] = food.errors.messages
+      render :index
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measure_unit, :price)
+  end
 end
