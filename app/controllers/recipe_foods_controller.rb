42ffe4d530f0adb8e0002105a711f09f8280a1d1
leonardo <leonardodias@ucl.br>
2021-11-16 14:35:57 -0300

    Create destroy, edit and update methods for recipe_foods.

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -15,10 +15,39 @@ class RecipeFoodsController < ApplicationController
 
     if recipe_food.save
       flash[:notice] = 'Ingredient added to recipe'
-      redirect_to recipes_path
+      redirect_to recipe_path(id: recipe.id)
     else
       flash[:error] = recipe_food.errors.messages
       render :new
+    end
+  end
+
+  def edit
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def update
+    recipe_food = RecipeFood.find(params[:id])
+    recipe = Recipe.find(params[:recipe_id])
+    if recipe_food.update(food_update_params)
+      flash[:notice] = 'Food modified.'
+      redirect_to recipe_path(id: recipe.id)
+    else
+      flash[:error] = recipe_food.errors.messages
+      render :index
+    end
+  end
+  
+  def destroy
+    recipe = Recipe.find(params[:recipe_id])
+    recipe_food = RecipeFood.find(params[:id])
+    if recipe_food.destroy
+      flash[:notice] = 'Food removed from recipe'
+      redirect_to recipe_path(id: recipe.id)
+    else
+      flash[:error] = recipe_food.errors.messages
+      render :index
     end
   end
 
@@ -26,5 +55,9 @@ class RecipeFoodsController < ApplicationController
   
   def recipe_food_params
     params.require(:recipe_food).permit(:quantity, :food_id)
+  end
+
+  def food_update_params
+    params.require(:recipe_food).permit(:quantity)
   end
 end
