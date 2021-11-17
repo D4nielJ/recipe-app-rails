c55ddaeaa52a3a9251b7f26a678b0ba63d4436a8
leonardo <leonardodias@ucl.br>
2021-11-17 17:57:24 -0300

    Create public form checkbox update method in recipe show page.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -28,6 +28,17 @@ class RecipesController < ApplicationController
     end
   end
 
+  def update
+    recipe = Recipe.find(params[:id])
+    if recipe.update(recipe_public_params)
+      flash[:notice] = 'Public modified.'
+      redirect_to recipe_path(id: recipe.id)
+    else
+      flash[:error] = recipe.errors.messages
+      render :index
+    end
+  end
+
   def destroy
     recipe = Recipe.find(params[:id])
     if recipe.destroy
@@ -43,5 +54,9 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
+
+  def recipe_public_params
+    params.require(:recipe).permit(:public)
   end
 end
