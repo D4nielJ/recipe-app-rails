602d95a08a40fa0fb0540a6ee96f4cad6adf8825
d4nielj <d4niel.djm@gmail.com>
2021-11-20 15:42:43 -0500

    Refactor method to calc how much food was used between all recipes

--- app/models/food.rb
+++ app/models/food.rb
@@ -8,4 +8,8 @@ class Food < ApplicationRecord
             inclusion: { in: ['Teaspoon (tsp)', 'Cup (c)', 'Unit (u)', 'Milliliter (ml)', 'Litter (l)',
                               'Milligram (mg)', 'Kilogram (kg)'] }
   validates :price, numericality: :greater_than_or_equal_to
+
+  def quantity_used
+    RecipeFood.joins(:food).where(food_id: id).sum('quantity')
+  end
 end
